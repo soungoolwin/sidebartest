d892300382e0fd94e4290b9d6c7f15e0dda33692
soungoolwin <soungoolwin275@gmail.com>
2021-01-19 17:12:00 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+v
+function show_hide()
+{
+    if(a==1)
+        {
+            document.getElementById("demo").style.display="inline";
+            return a=0;
+        }
+
+    else {
+        document.getElementById("demo").style.display="none";
+        return a=1;
+    }
+
+
+}
